f193a1de370d14d9ec641641b5f85fb06bc7de98
Lukasz Kopec <l.kopec.12@ucl.ac.uk>
2013-02-05 15:45:10 +0000

    Forgetting works (but slow)
    Forgetting added.

--- GCM_forgetting.py
+++ GCM_forgetting.py
@@ -20,7 +20,7 @@
 
 from random import random, gauss
 from os import path
-import math
+import sys,math
 
 class ps_data():
 
@@ -34,9 +34,9 @@ class ps_data():
         result=''
         try:
             result += ('Model with parameters: gamma: %(gamma)d, forget '+\
-                    'rate: %(forget_rate)d, choice parameter: '+\
-                    '%(choice_parameter)d, noise mean: %(noise_mu)d, '+\
-                    'noise sd: %(noise_sigma)d\n'+\
+                    'rate: %(forget_rate).5f, choice parameter: '+\
+                    '%(choice_parameter)d, noise mean: %(noise_mu).5f, '+\
+                    'noise sd: %(noise_sigma).5f\n'+\
                     'Data file used: %(datafile)s.\n') % self.__dict__
         except:
             result += 'Unparametrised GCM.\n'
@@ -83,8 +83,14 @@ class ps_data():
                             idealcat=1
                         # ^ this is the category which the ideal classifier
                         # would put the stimulus in.
-                        self.AddPs(int(a[0]),int(a[3]),int(a[1]),int(a[2]),int(a[4]),\
-                                actualcat, idealcat, pscat)
+                        self.AddPs(**{'ps_id': int(a[0]),\
+                                'trial_no': int(a[3]),\
+                                'session': int(a[1]),\
+                                'condition': int(a[2]),\
+                                'length': int(a[4]),\
+                                'actualCat': actualcat,\
+                                'idealCat': idealcat,\
+                                'responseCat': pscat})
                     except:
                         continue # say, if first line or something wrong
         else:
@@ -97,18 +103,11 @@ class ps_data():
         return int('%09d%03d%02d%02d' % \
                 (int(ps_id),int(trial_no),int(session),int(condition)))
 
-    def AddPs(self, ps_id, trial_no, session, condition, length, actualCat,\
-            idealCat, responseCat):
+    def AddPs(self, **kwargs):
         """ Here we add in a datapoint for a participant. Can also use that for
         updating the Ps's data. """
-        self.data[self.GetInstNo(ps_id,trial_no,session,condition)]={'ps_id':ps_id,\
-                'trial_no': trial_no,\
-                'session': session,\
-                'condition': condition,\
-                'length': self.AddNoise(length),\
-                'actualCat': actualCat,\
-                'idealCat': idealCat,\
-                'responseCat': responseCat}
+        self.data[self.GetInstNo(kwargs['ps_id'],kwargs['trial_no'],\
+                kwargs['session'],kwargs['condition'])]=kwargs
 
     def GetInstancesForPs(self, **kwargs):
         """Returns all instance ids for which the kwargs are matching with the
@@ -139,14 +138,19 @@ class ps_data():
     def WriteOut(self,fname):
         """Writes out the data to the file fname"""
         with open(fname,'w') as f:
-            f.write(self)
+            f.write(self.__repr__())
             f.write('ps_id\ttrial_no\tsession\tcondition\t'+\
                     'length\tactualCat\tidealCat\tresponseCat\t'+\
-                    'predictedCat\n')
+                    'modelledCat\n')
             for key in sorted(self.data.keys()):
-                f.write(('(ps_id)\t(trial_no)\t(session)\t(condition)\t'+\
-                        '(length)\t(actualCat)\t(idealCat)\t(responseCat)\t')%self.GetPsData(key)+\
-                        self.PredictCategory(key)+'\n')
+                try:
+                    f.write(('(ps_id)\t(trial_no)\t(session)\t(condition)\t'+\
+                        '(length)\t(actualCat)\t(idealCat)\t(responseCat)\t'+\
+                        '(modelledCat)\n') % self.GetPsData(key))
+                except:
+                    f.write(('(ps_id)\t(trial_no)\t(session)\t(condition)\t'+\
+                        '(length)\t(actualCat)\t(idealCat)\t(responseCat)\t')\
+                        %self.GetPsData(key) + self.PredictCategory(key)+'\n')
 
     # MODEL
     def SetParameters(self, gamma, forget_rate, choice_parameter,\
@@ -159,50 +163,58 @@ class ps_data():
         noise ~ Gaussian(mean,sd).
         """
         self.gamma=gamma
-        self.forget_rate=forget_rate
+        if forget_rate==0.0:
+            self.forget_rate=1e-50
+            # so that we don't get division by zero, but still (virtually) no
+            # forgetting
+        else:
+            self.forget_rate=forget_rate
         self.choice_parameter=choice_parameter
         self.noise_mu=noise_mu
         self.noise_sigma=noise_sigma
 
     # FORGETTING
-    def ReEstimateCategory(self,instance_no):
-        """Re-estimate the category membership of the instance."""
-        inst=self.GetPsData[instance_no]
-        # delete number from its category - need to do that because else you
-        # would use the category information of current instance to re-estimate
-        # it.
-        cat_old=inst['actualCat']
-        inst['actualCat']=self.PredictCategory(instance_no)
-        self.reEstimated += 1 # Add one to the number of re-estimated
-        # instances
-        if inst['actualCat']!=cat_old:
-            self.changedCats +=1 # Add one instance to the
-            # number of instances which changed category
-            self.AddPs(**inst) # overwrites the datapoint
-            if inst['actualCat']==1:
-                self.catA.remove(instance_no)
-                self.catB.append(instance_no)
-            else:
-                self.catB.remove(instance_no)
-                self.catA.append(instance_no)
-
-    def ReEstimateLength(self, instance_no):
-        """Re-estimate the length of an instance based on category
-        membership.
-        The re-estimated length is the average length of stimuli in the
-        category."""
-
-        pass
-
-    def ForgetInstances(self):
+    def ForgetOnce(self):
         """Selects instances which are to be forgotten, following an
         exponential distribution, based on recency of presentation."""
-        self.reEstimated=0
-        self.changedCats=0
-        # Start counting from here. Possibly move it to a different place if I
-        # decide to start counting from somewhere else, say while modelling
-        # incrementally.
-        pass
+        forgetCatA=[(math.exp(-(1.0/self.forget_rate)*(len(self.catA)-i)), no) \
+                for (i,no) in enumerate(self.catA)]
+        forgetCatB=[(math.exp(-(1.0/self.forget_rate)*(len(self.catB)-i)), no) \
+                for (i,no) in enumerate(self.catB)]
+        for (prob, instNo) in forgetCatA:
+            if random() < prob:
+                self.ReEstimateCategory(instNo)
+        for (prob, instNo) in forgetCatB:
+            if random() < prob:
+                self.ReEstimateCategory(instNo)
+
+    def ForgetLoop(self):
+        """ Loops through the whole dataset and after presenting every
+        instance goes through the cycle of forgetting."""
+        self.catA=[]
+        self.catB=[]
+        if self.verbose > 10:
+            print "Forgetting loop"
+            sys.stdout.write("[%s]" % (" " * 20))
+            sys.stdout.flush()
+            sys.stdout.write("\b" * (20+1))
+        for (i,instId) in enumerate(sorted(self.data.keys())):
+            inst=self.GetPsData(instId)
+            try:
+                cat=inst['modelledCat']
+            except:
+                cat=inst['actualCat']
+            if cat==-1:
+                self.catA.append(instId)
+            else:
+                self.catB.append(instId)
+            self.ForgetOnce()
+            if self.verbose > 10:
+                if (i % (len(self.data.keys())/20)) == 0:
+                    sys.stdout.write("-")
+                    sys.stdout.flush()
+        if verbose > 10:
+            sys.stdout.write("\n")
 
     # PERCEPTUAL NOISE
     def AddNoise(self, length):
@@ -221,13 +233,60 @@ class ps_data():
         distance."""
         return math.exp(-self.choice_parameter*math.sqrt((length1-length2)**2))
 
+    def ReEstimateCategory(self,instance_no):
+        """Re-estimate the category membership of the instance."""
+        inst=self.GetPsData(instance_no)
+        # delete number from its category - need to do that because else you
+        # would use the category information of current instance to re-estimate
+        # it.
+        try:
+            cat_old=inst['modelledCat'] # If the instance was already
+            # re-estimated using the GCM.
+        except:
+            cat_old=inst['actualCat'] # If it has not been modelled before
+        inst['modelledCat']=self.PredictCategory(instance_no)
+        try:
+            self.reEstimated += 1 # Add one to the number of re-estimated
+        except:
+            self.reEstimated = 1
+        # instances
+        if inst['modelledCat']!=cat_old:
+            try:
+                self.changedCats +=1 # Add one instance to the
+                # number of instances which changed category
+            except:
+                self.changedCats = 1
+            self.AddPs(**inst) # overwrites the datapoint
+            if cat_old==-1:
+                self.catA.remove(instance_no)
+                self.catB.append(instance_no)
+            else:
+                self.catB.remove(instance_no)
+                self.catA.append(instance_no)
+
+    def ReEstimateLength(self, instance_no):
+        """Re-estimate the length of an instance based on category
+        membership.
+        The re-estimated length is the average length of stimuli in the
+        category."""
+
+        pass
+
     def PredictCategory(self, instanceId):
         """Return the category membership of the instance.
         Assume categories are weighted equally. The gamma parameter is taken
         from the model parameter initialisation.
         Category A == -1
         Category B == 1"""
-        if (len(self.catA)==0 and len(self.catB)==0):
+        try:
+            self.catA
+        except:
+            self.catA=[]
+        try:
+            self.catB
+        except:
+            self.catB=[]
+        if (len(self.catA)<=1 and len(self.catB)<=1):
             if self.verbose>0:
                 print "Warning, trying to predict category on an "+\
                         "un-initialised model. Defaulting to a random "+\
@@ -237,18 +296,17 @@ class ps_data():
             else:
                 return 1
         else:
-            psData=self.GetPsData(instanceId)['length']
+            psData=self.GetPsData(instanceId)
             length=psData['length']
-            catA=self.catA
-            catB=self.catB
-            if psData['actualCat']==-1:
-                catA.remove(instanceId)
-            else:
-                catB.remove(instanceId)
+            try:
+                cat=psData['modelledCat']
+            except:
+                cat=psData['actualCat'] # If the instance has not been
+                # re-estimated
             sum_cat_A=sum([self.Similarity(length, self.GetPsData(inst)['length']) for \
-                inst in catA])
+                inst in self.catA if inst != instanceId])
             sum_cat_B=sum([self.Similarity(length, self.GetPsData(inst)['length']) for \
-                inst in catB])
+                inst in self.catB if inst != instanceId])
             prob_a=sum_cat_A**self.gamma/(sum_cat_A**self.gamma+sum_cat_B**self.gamma)
             prob_b=sum_cat_B**self.gamma/(sum_cat_A**self.gamma+sum_cat_B**self.gamma)
             if prob_a>=prob_b:
@@ -258,13 +316,14 @@ class ps_data():
 
     def TrainGCM(self):
         """Train the GCM incrementally using all instances."""
-        # First, 
-
+        # First, present the instances to the model one by one.
+        # Next, estimate the model response.
+        # Then forget and re-estimate
         pass
 
     def __init__(self):
         """For testing. """
-        self.SetParameters(1,0.001,1,0,0.5)
+        self.SetParameters(1,0.7,1,0,0.5)
         self.verbose=100
         # Add some instances.
         pass
