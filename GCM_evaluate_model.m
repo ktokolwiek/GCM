7c6affc93f8692f1c2311d05aa18f7ef0be9c2e1
Lukasz Kopec <the.ktokolwiek@gmail.com>
2013-02-26 16:04:27 +0000

    First results

--- GCM_evaluate_model.m
+++ GCM_evaluate_model.m
@@ -1,6 +1,6 @@
 function GCM_evaluate_model(fType, gammas, forget_rates, noise_mu, noise_sigmas, choice_parameters)
 %% Wrapper for testing the GCM model in GCM_model.m
-no_repeats = 1;
+no_repeats = 4;
 
 no_combinations = length(gammas)*length(forget_rates)*length(noise_sigmas)*...
     length(choice_parameters);
@@ -13,33 +13,39 @@ header_test={'subj','session','feedType','trial','length_avg','length_sd','respC
 %% Parallel for loop
 
 combination = 0;
-matlabpool open 1 % on the love01 machine
+matlabpool open 12 % on the love01 machine
 fprintf('Progress is %2.0f%%',0)
 for gamma = gammas
     for forget_rate = forget_rates
         for noise_sigma = noise_sigmas
             for choice_parameter = choice_parameters
                 lls = zeros(no_repeats, 1);
-                traind = [];
-                testd = [];
-                parfor iter = 1:no_repeats
+                parfor iter = 1:no_repeats-1
                     [trainData, lls(iter), testData] = GCM_model('gamma', gamma, 'forget_rate',...
                         forget_rate, 'choice_parameter', choice_parameter,...
                         'noise_mu', noise_mu, 'noise_sigma', noise_sigma,...
-                        'feedType', fType, 'verbose', 100);
+                        'feedType', fType, 'verbose', -1);
                     train_lengths(:,iter) = trainData(:,5);
                     train_ideal(:,iter) = trainData(:,8);
                     train_model(:,iter) = trainData(:,9);
                     test_lengths(:,iter) = testData(:,5);
                     test_model(:,iter) = testData(:,8);
-                    traind = [];
-                    testd = [];
                     % progress bar
                     fprintf(repmat('\b',1,length('Progress is 20p')));
-                    fprintf('Progress is %2.0f%%',(combination*100*no_repeats+iter)...
-                        /(no_combinations*no_repeats));
+                    fprintf('Progress is %2.0f%%',(combination*no_repeats+iter)...
+                        /(no_combinations*no_repeats*0.01));
                     
                 end
+		[trainData, lls(no_repeats), testData] = GCM_model('gamma', gamma, 'forget_rate',...
+                        forget_rate, 'choice_parameter', choice_parameter,...
+                        'noise_mu', noise_mu, 'noise_sigma', noise_sigma,...
+                        'feedType', fType, 'verbose', -1);
+		train_lengths(:,no_repeats) = trainData(:,5);
+		train_ideal(:,no_repeats) = trainData(:,8);
+		train_model(:,no_repeats) = trainData(:,9);
+		test_lengths(:,no_repeats) = testData(:,5);
+		test_model(:,no_repeats) = testData(:,8);
+
                 combination = combination + 1;
                 %% get the filenames
                 if fType == 1
@@ -54,25 +60,25 @@ for gamma = gammas
                         gamma, forget_rate, noise_sigma, choice_parameter);
                 end
                 %% prepare the data
-                train_results = [traind(:,1:4) mean(train_lengths')' std(train_lengths')' ...
-                    traind(:,6:7) mean(train_ideal')' std(train_ideal')' ...
+                train_results = [trainData(:,1:4) mean(train_lengths')' std(train_lengths')' ...
+                    trainData(:,6:7) mean(train_ideal')' std(train_ideal')' ...
                     mean(train_model')' std(train_model')'];
-                test_results = [testd(:,1:4) mean(test_lengths')' std(test_lengths')' ...
-                    testd(:,6) mean(test_model')' std(test_model')'];
+                test_results = [testData(:,1:4) mean(test_lengths')' std(test_lengths')' ...
+                    testData(:,6) mean(test_model')' std(test_model')'];
                 %% write out the results
-                nrows=length(traind(:,1));
+                nrows=length(train_results(:,1));
                 fid = fopen(train_fname, 'w');
                 fprintf(fid, '%s,%s,%s,%s,%s,%s,%s,%s,%s\n', header_train{:});
                 for row=1:nrows
-                    fprintf(fid, '%d,%d,%d,%d,%.2f,%.2f,%d,%.2f,%.2f\n', test_results(row,:));
+                    fprintf(fid, '%d,%d,%d,%d,%.2f,%.2f,%d,%.2f,%.2f\n', train_results(row,:));
                 end
                 fclose(fid);
                 
-                nrows=length(testd(:,1));
+                nrows=length(test_results(:,1));
                 fid = fopen(test_fname, 'w');
                 fprintf(fid, '%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s\n', header_test{:});
                 for row=1:nrows
-                    fprintf(fid, '%d,%d,%d,%d,%.2f,%.2f,%d,%d,%.2f,%.2f,%.2f,%.2f\n', train_results(row,:));
+                    fprintf(fid, '%d,%d,%d,%d,%.2f,%.2f,%d,%d,%.2f,%.2f,%.2f,%.2f\n', test_results(row,:));
                 end
                 fclose(fid);
             end
@@ -82,17 +88,6 @@ end
 
 
 %% write out the results
-
-[nrows,~]= size(C);
-
-fid = fopen(filename, 'w');
-
-fprintf(fid, '%s,%s,%s,%s,%s,%s,%s\n', C{1,:});
-for row=2:nrows
-    fprintf(fid, '%.1f,%.10f,%.1f,%.1f,%.1f,%.7f,%.7f\n', C{row,:});
-end
-
-fclose(fid);
 
 matlabpool close
 end
