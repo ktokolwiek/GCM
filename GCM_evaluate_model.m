96079f3292de2c09111674fcec7210c6daae5cd2
Lukasz Kopec <the.ktokolwiek@gmail.com>
2013-02-27 11:19:20 +0000

    Modified GCM_evaluate_model so that is read data in again instead of doing one non-parallel job, and also fixed data output using dlmwrite.

--- GCM_evaluate_model.m
+++ GCM_evaluate_model.m
@@ -1,14 +1,12 @@
 function GCM_evaluate_model(fType, gammas, forget_rates, noise_mu, noise_sigmas, choice_parameters)
 %% Wrapper for testing the GCM model in GCM_model.m
-no_repeats = 4;
+no_repeats = 100;
 
 no_combinations = length(gammas)*length(forget_rates)*length(noise_sigmas)*...
     length(choice_parameters);
 % the headers
-header_train={'subj','session','feedType','trial','length_avg','length_sd','tarCat','respCat',...
-    'idealCat_avg','idealCat_sd','modelledCat_avg','modelledCat_sd'};
-header_test={'subj','session','feedType','trial','length_avg','length_sd','respCat',...
-    'modelledCat_avg','modelledCat_sd'};
+header_train={'subj','session','feedType','trial','length_avg','length_sd','tarCat','respCat', 'idealCat','modelledCat_avg','modelledCat_sd'};
+header_test={'subj','session','feedType','trial','length_avg','length_sd','respCat', 'modelledCat_avg','modelledCat_sd'};
 
 %% Parallel for loop
 
@@ -20,13 +18,12 @@ for gamma = gammas
         for noise_sigma = noise_sigmas
             for choice_parameter = choice_parameters
                 lls = zeros(no_repeats, 1);
-                parfor iter = 1:no_repeats-1
+                parfor iter = 1:no_repeats
                     [trainData, lls(iter), testData] = GCM_model('gamma', gamma, 'forget_rate',...
                         forget_rate, 'choice_parameter', choice_parameter,...
                         'noise_mu', noise_mu, 'noise_sigma', noise_sigma,...
                         'feedType', fType, 'verbose', -1);
                     train_lengths(:,iter) = trainData(:,5);
-                    train_ideal(:,iter) = trainData(:,8);
                     train_model(:,iter) = trainData(:,9);
                     test_lengths(:,iter) = testData(:,5);
                     test_model(:,iter) = testData(:,8);
@@ -36,17 +33,13 @@ for gamma = gammas
                         /(no_combinations*no_repeats*0.01));
                     
                 end
-		[trainData, lls(no_repeats), testData] = GCM_model('gamma', gamma, 'forget_rate',...
-                        forget_rate, 'choice_parameter', choice_parameter,...
-                        'noise_mu', noise_mu, 'noise_sigma', noise_sigma,...
-                        'feedType', fType, 'verbose', -1);
-		train_lengths(:,no_repeats) = trainData(:,5);
-		train_ideal(:,no_repeats) = trainData(:,8);
-		train_model(:,no_repeats) = trainData(:,9);
-		test_lengths(:,no_repeats) = testData(:,5);
-		test_model(:,no_repeats) = testData(:,8);
-
-                combination = combination + 1;
+		%% Read the files again so that we have the raw data for output like subject ID, feedback type etc.
+		trainData = xlsread('training_all.xls');
+		testData = xlsread('test_all.xls');
+		trainData(:,8) = 2*(trainData(:,5)>30.5)-1; % -1 for cat A (short), 1 for cat B (long)
+		trainData = [trainData trainData(:,6)]; % copy the feedback to modelled category.
+                
+		combination = combination + 1;
                 %% get the filenames
                 if fType == 1
                     train_fname = sprintf('GCM_results/actual_training_%.1f_%.15f_%.1f_%.1f.csv',...
@@ -66,28 +59,20 @@ for gamma = gammas
                 test_results = [testData(:,1:4) mean(test_lengths')' std(test_lengths')' ...
                     testData(:,6) mean(test_model')' std(test_model')'];
                 %% write out the results
-                nrows=length(train_results(:,1));
                 fid = fopen(train_fname, 'w');
-                fprintf(fid, '%s,%s,%s,%s,%s,%s,%s,%s,%s\n', header_train{:});
-                for row=1:nrows
-                    fprintf(fid, '%d,%d,%d,%d,%.2f,%.2f,%d,%.2f,%.2f\n', train_results(row,:));
-                end
-                fclose(fid);
+                fprintf(fid, '%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s\n', header_train{:});
+                fclose(fid)
+		dlmwrite(train_fname,train_results,'-append', 'precision','%.2f');
                 
-                nrows=length(test_results(:,1));
                 fid = fopen(test_fname, 'w');
-                fprintf(fid, '%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s\n', header_test{:});
-                for row=1:nrows
-                    fprintf(fid, '%d,%d,%d,%d,%.2f,%.2f,%d,%d,%.2f,%.2f,%.2f,%.2f\n', test_results(row,:));
-                end
+                fprintf(fid, '%s,%s,%s,%s,%s,%s,%s,%s,%s\n', header_test{:});
                 fclose(fid);
+		dlmwrite(train_fname,test_results,'-append', 'precision','%.2f');
             end
         end
     end
 end
 
-
-%% write out the results
 
 matlabpool close
 end
