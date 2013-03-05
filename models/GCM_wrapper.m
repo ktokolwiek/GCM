660d9d81c4219f1c4ae0870de8bc596ec74f0da2
Lukasz Kopec <the.ktokolwiek@gmail.com>
2013-03-05 12:03:16 +0000

    Changed the directory structure, hope now things are clearer.

---
+++ models/GCM_wrapper.m
@@ -0,0 +1,73 @@
+function GCM_wrapper(fType)
+%% Wrapper for testing the GCM model in GCM_model.m
+gammas = [0.5 1 2];
+forget_rates = logspace(-15,-7,5);
+noise_mu = 0;
+noise_sigmas = [0.1 0.5];
+choice_parameters = [1 2];
+no_repeats = 10;
+
+no_combinations = length(gammas)*length(forget_rates)*length(noise_sigmas)*...
+    length(choice_parameters);
+
+C={'gamma', 'forget_rate', 'noise_mu', 'noise_sigma', 'choice_parameter',...
+    'll_mean', 'll_sd'}; % the headers
+%% Parallel for loop
+
+combination = 2;
+matlabpool open 12 % on the love01 machine
+fprintf('Progress is %2.0f%%',0)
+for gamma = gammas
+    for forget_rate = forget_rates
+        for noise_sigma = noise_sigmas
+            for choice_parameter = choice_parameters
+                lls = zeros(no_repeats,1);
+                parfor iter = 1:no_repeats
+                    [~, lls(iter)] = GCM_model('gamma', gamma, 'forget_rate',...
+                        forget_rate, 'choice_parameter', choice_parameter,...
+                        'noise_mu', noise_mu, 'noise_sigma', noise_sigma,...
+                        'feedType', fType, 'verbose', -1);
+                end
+                C(combination,:) = {gamma, forget_rate, noise_mu,...
+                    noise_sigma, choice_parameter, mean(lls), std(lls)};
+		fprintf(repmat('\b',1,length('Progress is 20p')));
+		fprintf('Progress is %2.0f%%',(combination-1.0)*100/no_combinations)
+                combination = combination + 1;
+		%% write out the results every iteration so that we don't lose anything
+		% If we stop execution.
+
+		[nrows,~]= size(C);
+		if fType == 1
+			filename = 'actual_feedback.csv';
+		else
+			filename = 'ideal_feedback.csv';
+		end
+		fid = fopen(filename, 'w');
+
+		fprintf(fid, '%s,%s,%s,%s,%s,%s,%s\n', C{1,:});
+		for row=2:nrows
+		    fprintf(fid, '%.1f,%.15f,%.1f,%.1f,%.1f,%.7f,%.7f\n', C{row,:});
+		end
+
+		fclose(fid);
+            end
+        end
+    end
+end
+
+
+%% write out the results
+
+[nrows,~]= size(C);
+
+fid = fopen(filename, 'w');
+
+fprintf(fid, '%s,%s,%s,%s,%s,%s,%s\n', C{1,:});
+for row=2:nrows
+    fprintf(fid, '%.1f,%.15f,%.1f,%.1f,%.1f,%.7f,%.7f\n', C{row,:});
+end
+
+fclose(fid);
+
+matlabpool close
+end
---
+++ models/testcase_1.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+# 7 February
+# Simple test case for the model.
+
+import GCM_forgetting as GCM
+import numpy as np
+from multiprocessing import Pool
+
+
+def testOneModel((gamma, forget_rate, choice_parameter, noise_mu,\
+        noise_sigma)):
+    """ Performs evaluation of one model with given parameters. Returns the log
+    likelihood of the model fit.
+    """
+    fname = 'Randall_learn_lean_dataset.csv'
+    model = GCM.ps_data(fname, 15, gamma, forget_rate, choice_parameter, \
+            noise_mu, noise_sigma)
+    instances = model.GetInstancesForPs(**{'condition': 1})
+    model.PresentLoop(instances)
+    return model.logLikelihood
+    pass
+
+def writeout(logLikelihoods, parameters, fname):
+    """ Writes out the parameters and associated log likelihoods to file fname
+    """
+    with open(fname,'w') as f:
+        f.write('log_likelihood,gamma,forget_rate,choice_parameter,noise_mu,noise_sigma')
+        for (ll, params) in zip(logLikelihoods, parameters):
+            f.write('%.5f,%.5f,%.5f,%.5f,%.5f,%.5f\n' % ((ll,)+params))
+
+def test():
+    gammas=[0.5, 1, 2]
+    forget_rates=np.logspace(-2,-.15, num=7)
+    choice_parameters=[1,2]
+    noise_mu=0
+    noise_sigmas=[0.1,0.5]
+    parameters = [(gamma, forget_rate, choice_parameter, noise_mu,\
+        noise_sigma) for gamma in gammas for forget_rate in forget_rates\
+        for choice_parameter in choice_parameters for noise_sigma\
+        in noise_sigmas]
+    pool=Pool(processes=20)
+    logLikelihoods=pool.map(testOneModel, parameters)
+    writeout(logLikelihoods, parameters, 'loglikelihoods_ideal.csv')
